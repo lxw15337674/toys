cbee8977a4247d556e329a2e84596d16ce5c5378
15515255978 <404174262@qq.com>
2020-04-21 20:31:56 +0800

    imgToCanvas.js 方法完善

--- js/imgBlob.js
+++ js/imgBlob.js
@@ -2,7 +2,7 @@
 /**
  * @description: 创建canvas元素，渲染远程图片，一般用于上传图片后显示、图片上传服务器
  * @param {string} url 图片地址 
- * @return: 
+ * @return: 返回元素
  */
 
 export default function imgBlob(imgUrl) {
@@ -15,10 +15,10 @@ export default function imgBlob(imgUrl) {
         c.height = img.height;
         let ctx = c.getContext('2d');
         ctx.drawImage(img, 0, 0);
-        return c
         // c.toBlob((blob) => {
         //     let formData = new FormData();
         //     formData.append('file', blob, 'test.jpg');
         // }, 'image/jpeg');
     };
+    return c
 }
